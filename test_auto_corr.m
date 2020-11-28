6cc40c1d1508abd68919b83e12701e868f23968a
entrnickvana <nick.jwl123@gmail.com>
2020-11-27 18:41:30 -0700

    added a implementation proof of autocorrelation as PSD in frequency domain

---
+++ test_auto_corr.m
@@ -0,0 +1,24 @@
+close all;
+clear all;
+len = 1024*8;
+wdw = 128*8;
+
+s = sign(rand(1, len)-.5);
+padding = zeros(1, len);
+
+x = [padding s padding];
+y = zeros(1, (2*wdw)-1)
+
+for ii = -1*wdw:1:wdw-1
+	temp = circshift(x,ii);
+	y(wdw+ii+1) = x(len+1:2*len+1)*transpose(temp(len+1:2*len+1))/((2*wdw)-1);
+end
+
+figure(1);
+plot(y);
+
+figure(2);
+plot(abs(fft(y)));
+
+
+
